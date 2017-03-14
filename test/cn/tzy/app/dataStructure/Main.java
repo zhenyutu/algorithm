34b21665172191559a18417fcb58eb9a0c20c467
zhenyutu <1572629866@qq.com>
2017-03-14 16:54:31 +0800

    添加junit测试类，对sequence类进行单元测试

---
+++ test/cn/tzy/app/dataStructure/Main.java
@@ -0,0 +1,7 @@
+package cn.tzy.app.dataStructure;
+
+/**
+ * Created by tuzhenyu on 17-3-14.
+ */
+public class Main {
+}
---
+++ test/cn/tzy/app/dataStructure/list/SequenceListTest.java
@@ -0,0 +1,52 @@
+package cn.tzy.app.dataStructure.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tuzhenyu on 17-3-14.
+ * @author tuzhenyu
+ */
+public class SequenceListTest {
+    SequenceList list1;
+    SequenceList list2;
+
+    @Before
+    public void init() throws Exception{
+        list1 = new SequenceList<Integer>();
+        int [] v= {1,2,3,9,5,8};
+        for(int i:v) {
+            list1.insert(i);
+        }
+
+        list2 = new SequenceList<String>();
+        String [] strs = {"hello","nice","nee you","mei guo"};
+        for(String s:strs) {
+            list2.insert(s);
+        }
+    }
+    @Test
+    public void test() throws Exception{
+        for (int i=0;i<list1.length();i++){
+            System.out.print(list1.get(i)+" ");
+        }
+        for (int i=0;i<list2.length();i++){
+            System.out.print(list2.get(i)+" ");
+        }
+
+        list1.insert(2,7);
+        list2.delete(2);
+        System.out.println("");
+
+        for (int i=0;i<list1.length();i++){
+            System.out.print(list1.get(i)+" ");
+        }
+        for (int i=0;i<list2.length();i++){
+            System.out.print(list2.get(i)+" ");
+        }
+    }
+}
