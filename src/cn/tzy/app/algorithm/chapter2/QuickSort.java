40c2d7b9bc252f04898f2213a19d4ea3e9c708f8
zhenyutu <1572629866@qq.com>
2017-04-15 17:01:49 +0800

    完成快速排序

---
+++ src/cn/tzy/app/algorithm/chapter2/QuickSort.java
@@ -0,0 +1,58 @@
+package cn.tzy.app.algorithm.chapter2;
+
+
+import java.util.Arrays;
+
+/**
+ * Created by tuzhenyu on 17-4-15.
+ * @author tuzhenyu
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        Integer[] a = {3,2,6,4,7,2,1};
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void sort(Comparable[] a){
+        sort(a,0,a.length-1);
+    }
+
+    public static void sort(Comparable[] a,int low,int high){
+        if (high<=low)
+            return;
+        int j = partition(a,low,high);
+        sort(a,low,j-1);
+        sort(a,j+1,high);
+    }
+
+    private static int partition(Comparable[] a,int low,int high){
+        int i=low,j=high+1;
+        Comparable v = a[low];
+        while (true){
+            while (less(a[++i],v)){
+                if (i>=high)
+                    break;
+            }
+            while (less(v,a[--j])){
+                if (j<=low)
+                    break;
+            }
+            if (i>=j)
+                break;
+            exch(a,i,j);
+        }
+        exch(a,low,j);
+        return j;
+    }
+
+    private static boolean less(Comparable v , Comparable w){
+        return v.compareTo(w)<0;
+    }
+
+    private static void exch(Comparable[] a,int i , int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
