d036eae217d8fb468baf87050ec82e8a455f37dc
zhenyutu <1572629866@qq.com>
2017-04-23 09:51:00 +0800

    实现二叉查找树的删除

--- src/cn/tzy/app/algorithm/chapter3/BST.java
+++ src/cn/tzy/app/algorithm/chapter3/BST.java
@@ -2,6 +2,7 @@ package cn.tzy.app.algorithm.chapter3;
 
 /**
  * Created by tuzhenyu on 17-4-19.
+ * @author tuzhenyu
  */
 public class BST<T extends Comparable<T>,V> {
     private Node root;
@@ -16,50 +17,93 @@ public class BST<T extends Comparable<T>,V> {
             this.v = v;
             this.N = N;
         }
+    }
 
-        public int size(Node node){
-            return node.N;
+    public int size(Node node){
+        return node.N;
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    public void put(T t,V v){
+        root = put(root,t,v);
+    }
+
+    private Node put(Node node,T t,V v){
+        if (node == null){
+            return new Node(t,v,1);
         }
-
-        public int size(){
-            return size(root);
+        int cmp = t.compareTo(node.t);
+        if (cmp>0){
+            node.right = put(node.right,t,v);
+        }else if (cmp<0){
+            node.left = put(node.left,t,v);
+        }else {
+            node.v = v;
         }
+        node.N = size(node.left)+ size(node.right) + 1;
+        return node;
+    }
 
-        public void put(T t,V v){
-            root = put(root,t,v);
+    public V get(T t){
+        return get(root,t);
+    }
+
+    private V get(Node node,T t){
+        if (t == null)
+            return null;
+        int cmp = t.compareTo(node.t);
+        if (cmp > 0) {
+            return get(node.right,t);
+        }else if(cmp<0){
+            return get(node.left,t);
+        }else {
+            return node.v;
         }
+    }
 
-        private Node put(Node node,T t,V v){
-            if (node == null){
-                return new Node(t,v,1);
-            }
-            int cmp = t.compareTo(node.t);
-            if (cmp>0){
-                node.right = put(node.right,t,v);
-            }else if (cmp<0){
-                node.left = put(node.left,t,v);
-            }else {
-                node.v = v;
-            }
-            node.N = size(node.left)+ size(right) + 1;
+    public V min(){
+        return min(root).v;
+    }
+
+    private Node min(Node node){
+        if (node.left == null)
             return node;
-        }
+        return min(node.left);
+    }
 
-        public V get(T t){
-            return get(root,t);
-        }
+    public void deleteMin(){
+        deleteMin(root);
+    }
 
-        private V get(Node node,T t){
-            if (t == null)
-                return null;
-            int cmp = t.compareTo(node.t);
-            if (cmp > 0) {
-                return get(node.right,t);
-            }else if(cmp<0){
-                return get(node.left,t);
-            }else {
-                return node.v;
-            }
+    private Node deleteMin(Node node){
+        if (node.left == null)
+            return node.right;
+        else
+            node.left = deleteMin(node.left);
+        return node;
+    }
+
+    private Node delete(Node node,T t){
+        if (node == null)
+            return null;
+        int cmp = t.compareTo(node.t);
+        if (cmp>0)
+            return node.right = delete(node.right,t);
+        else if (cmp<0)
+            return node.left = delete(node.left,t);
+        else {
+            if (node.left == null)
+                return node.right;
+            if (node.right == null)
+                return node.left;
+            Node tmp = node;
+            node = min(tmp.right);
+            node.right = deleteMin(tmp.right);
+            node.left = tmp.left;
         }
+        return node;
     }
 }
---
+++ src/cn/tzy/app/algorithm/chapter3/RedBlackBST.java
@@ -0,0 +1,9 @@
+package cn.tzy.app.algorithm.chapter3;
+
+/**
+ * Created by tuzhenyu on 17-4-23.
+ * @author tuzhenyu
+ */
+public class RedBlackBST {
+
+}
