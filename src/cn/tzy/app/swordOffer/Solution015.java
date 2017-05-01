14ce3c55f462572fe002af18398f9f9694a49f60
zhenyutu <1572629866@qq.com>
2017-05-01 08:42:58 +0800

    优化数组的奇偶分组

--- src/cn/tzy/app/swordOffer/Solution015.java
+++ src/cn/tzy/app/swordOffer/Solution015.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
  */
 public class Solution015 {
     /**
+     * 利用插入排序思想
      *移动数组中的奇数和偶数，最直观的方法是从头开始遍历，奇数往前移动，偶数跳过；
      * 37ms 654k
      */
@@ -22,6 +23,25 @@ public class Solution015 {
             }
         }
     }
+    /**
+     * 利用快排思想
+     *维护两个指针分别寻找奇数和偶数，无序
+     * 可以用于不许保证顺序的分割
+     */
+    public static void reOrderArray2(int [] array) {
+        int odd = 0;
+        int even = 0;
+
+        while (even<array.length&&odd<array.length){
+            while (array[odd]%2==1)
+                odd++;
+            while (array[even]%2==0)
+                even++;
+            exchange(array,odd,even);
+            odd++;
+            even++;
+        }
+    }
 
     private static void exchange(int[] a , int i , int j){
         int t = a[i];
@@ -31,7 +51,7 @@ public class Solution015 {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        reOrderArray(arr);
+        reOrderArray2(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
