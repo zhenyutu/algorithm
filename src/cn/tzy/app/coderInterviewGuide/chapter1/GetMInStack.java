c20fde89166670b7690b414643b534d3371f8fe4
zhenyutu <1572629866@qq.com>
2017-07-11 20:23:06 +0800

    完成getMIn的栈

---
+++ src/cn/tzy/app/coderInterviewGuide/chapter1/GetMInStack.java
@@ -0,0 +1,52 @@
+package cn.tzy.app.coderInterviewGuide.chapter1;
+
+import java.util.Stack;
+
+/**
+ * Created by tuzhenyu on 17-7-11.
+ * @author tuzhenyu
+ */
+public class GetMInStack {
+    private Stack<Integer> stackData = new Stack<>();
+    private Stack<Integer> stackMin = new Stack<>();
+
+    public void push(Integer t){
+        stackData.push(t);
+        if (stackMin.isEmpty()){
+            stackMin.push(t);
+        }else if (stackMin.peek()>t){
+            stackMin.push(t);
+        }
+    }
+
+    public Integer pop(){
+        int tmp = stackData.pop();
+        if (stackMin.peek() == tmp)
+            stackMin.pop();
+        return tmp;
+    }
+
+    public Integer getMin(){
+        return stackMin.peek();
+    }
+
+    public static void main(String[] args) {
+        GetMInStack stack = new GetMInStack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(4);
+        stack.push(28);
+        stack.push(6);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.getMin());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.getMin());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.getMin());
+    }
+
+}
---
+++ src/cn/tzy/app/swordOffer/Solution033.java
@@ -0,0 +1,13 @@
+package cn.tzy.app.swordOffer;
+
+/**
+ * Created by tuzhenyu on 17-5-27.
+ * @author tuzhenyu
+ */
+public class Solution033 {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int num = n;
+
+        return num;
+    }
+}
