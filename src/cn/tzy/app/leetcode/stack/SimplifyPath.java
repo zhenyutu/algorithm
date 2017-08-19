1251925d8a1d25125a0f74f9115ec268def6c29e
zhenyutu <1572629866@qq.com>
2017-08-19 08:36:33 +0800

    完成简化路径

---
+++ src/cn/tzy/app/leetcode/stack/SimplifyPath.java
@@ -0,0 +1,36 @@
+package cn.tzy.app.leetcode.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * Created by tuzhenyu on 17-8-19.
+ * @author tuzhenyu
+ * leetcode_71
+ */
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String result = "";
+        String[] paths = path.split("/");
+        for (String p : paths){
+            if ("..".equals(p)){
+                if (!stack.isEmpty())
+                    stack.pop();
+            }else if (".".equals(p)||"".equals(p))
+                continue;
+            else
+                stack.push(p);
+        }
+        while (!stack.isEmpty()){
+            result = "/" + stack.pop() + result;
+        }
+        return "".equals(result)?"/":result;
+    }
+
+    public static void main(String[] args) {
+        SimplifyPath s = new SimplifyPath();
+//        System.out.println(s.simplifyPath("/a/./b/../../c/"));
+        System.out.println(s.simplifyPath("/a/./b///../c/../././../d/..//../e/./f/./g/././//.//h///././/..///"));
+    }
+}
