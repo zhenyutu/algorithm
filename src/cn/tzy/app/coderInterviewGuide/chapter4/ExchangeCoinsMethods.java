e11e1998453836e450ced13c7fb3e80688eb79c4
zhenyutu <1572629866@qq.com>
2017-09-27 09:22:38 +0800

    完成凑钱方法的记忆搜索改进

--- src/cn/tzy/app/coderInterviewGuide/chapter4/ExchangeCoinsMethods.java
+++ src/cn/tzy/app/coderInterviewGuide/chapter4/ExchangeCoinsMethods.java
@@ -23,9 +23,35 @@ public class ExchangeCoinsMethods {
         return res;
     }
 
+    public int exchange2(int[] coins,int target){
+        if (coins==null||coins.length==0||target<0)
+            return 0;
+        int[][] map = new int[coins.length+1][target+1];
+        return getTarget2(coins,0,target,map);
+    }
+
+    private int getTarget2(int[] coins,int index,int target,int[][] map){
+        int res = 0;
+        if (index==coins.length)
+            res = target==0?1:0;
+        else {
+            int mapValue = 0;
+            for (int i=0;coins[index]*i<=target;i++){
+                mapValue = map[index+1][target-coins[index]*i];
+                if (mapValue!=0){
+                    res += mapValue==-1?0:mapValue;
+                }else {
+                    res += getTarget2(coins,index+1,target-coins[index]*i,map);
+                }
+            }
+        }
+        map[index][target] = res==0?-1:res;
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] coins = {5,10,25,1};
         ExchangeCoinsMethods e = new ExchangeCoinsMethods();
-        System.out.println(e.exchange(coins,15));
+        System.out.println(e.exchange2(coins,15));
     }
 }
