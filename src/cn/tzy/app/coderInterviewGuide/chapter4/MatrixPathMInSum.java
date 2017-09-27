17741e4107463f0f944471c18f2a8902935e344c
zhenyutu <1572629866@qq.com>
2017-09-27 08:35:06 +0800

    完成递归的记忆搜索

--- src/cn/tzy/app/coderInterviewGuide/chapter4/MatrixPathMInSum.java
+++ src/cn/tzy/app/coderInterviewGuide/chapter4/MatrixPathMInSum.java
@@ -1,5 +1,7 @@
 package cn.tzy.app.coderInterviewGuide.chapter4;
 
+import java.util.Arrays;
+
 /**
  * Created by tuzhenyu on 17-9-25.
  * @author tuzhenyu
@@ -9,6 +11,13 @@ public class MatrixPathMInSum {
         return getMinSum(arr,0,0);
     }
 
+    /**
+     * 暴力递归
+     * @param arr
+     * @param i
+     * @param j
+     * @return
+     */
     private int getMinSum(int[][] arr,int i,int j){
         if (i==arr.length-1&&j==arr.length-1){
             return arr[i][j];
@@ -25,12 +34,42 @@ public class MatrixPathMInSum {
             rm = Integer.MAX_VALUE;
 
         return bm<rm?bm+arr[i][j]:rm+arr[i][j];
+    }
 
+    public int minPathSum2(int[][] arr){
+        if (arr==null||arr.length==0||arr[0].length==0)
+            return 0;
+        int[][] dp = new int[arr.length][arr[0].length];
+        for (int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return getMinSum2(arr,0,0,dp);
+    }
+
+    private int getMinSum2(int[][] arr,int i,int j,int[][] dp){
+        if (dp[i][j]>=0)
+            return dp[i][j];
+        if (i==arr.length-1&&j==arr.length-1){
+            return arr[i][j];
+        }
+        int bm,rm;
+        if (i<arr.length-1)
+            bm = getMinSum(arr,i+1,j);
+        else
+            bm = Integer.MAX_VALUE;
+
+        if (j<arr.length-1)
+            rm = getMinSum(arr,i,j+1);
+        else
+            rm = Integer.MAX_VALUE;
+
+        dp[i][j] = bm<rm?bm+arr[i][j]:rm+arr[i][j];
+        return dp[i][j];
     }
 
     public static void main(String[] args) {
         MatrixPathMInSum m = new MatrixPathMInSum();
         int[][] arr = {{1,3,5,9},{8,1,3,4},{5,0,6,1},{8,8,4,0}};
-        System.out.println(m.minPathSum(arr));
+        System.out.println(m.minPathSum2(arr));
     }
 }
