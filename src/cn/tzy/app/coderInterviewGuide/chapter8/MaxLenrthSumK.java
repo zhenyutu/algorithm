16dcbdf2b0bef5a078ddff2bc5c4fd0a05b92bdb
zhenyutu <1572629866@qq.com>
2017-10-19 08:45:08 +0800

    最长子数组

--- src/cn/tzy/app/coderInterviewGuide/chapter8/MaxLenrthSumK.java
+++ src/cn/tzy/app/coderInterviewGuide/chapter8/MaxLenrthSumK.java
@@ -2,6 +2,29 @@ package cn.tzy.app.coderInterviewGuide.chapter8;
 
 /**
  * Created by tuzhenyu on 17-10-19.
+ * @author tuzhenyu
  */
 public class MaxLenrthSumK {
+    public int maxLength(int[] arr,int k){
+        if (arr==null||arr.length==0)
+            return 0;
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int len = 0;
+        while (right<arr.length){
+            if (sum==k){
+                len = Math.max(len,right-left+1);
+                sum-=arr[left++];
+            }else if (sum<k){
+                right++;
+                if (right==arr.length)
+                    break;
+                sum+=arr[right];
+            }else {
+                sum-=arr[left++];
+            }
+        }
+        return len;
+    }
 }
