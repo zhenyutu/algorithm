5ead9247d87e343ed40902059db3d9e75d712794
zhenyutu <1572629866@qq.com>
2017-10-22 08:39:03 +0800

    完成未排序数组非全正值的定长最长子数组

--- src/cn/tzy/app/coderInterviewGuide/chapter8/MaxLenrthSumK.java
+++ src/cn/tzy/app/coderInterviewGuide/chapter8/MaxLenrthSumK.java
@@ -1,10 +1,18 @@
 package cn.tzy.app.coderInterviewGuide.chapter8;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by tuzhenyu on 17-10-19.
  * @author tuzhenyu
  */
+
 public class MaxLenrthSumK {
+    /**
+     * 数组全部为正值
+     * O(n) O(1)
+     */
     public int maxLength(int[] arr,int k){
         if (arr==null||arr.length==0)
             return 0;
@@ -27,4 +35,28 @@ public class MaxLenrthSumK {
         }
         return len;
     }
+
+    /**
+     *数组中的值正负不定不能通过左右指针，只能通过辅助map
+     * O(n) O(n)
+     */
+    public int maxLength2(int[] arr,int k){
+        if (arr==null||arr.length==0)
+            return 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+        for (int i=0;i<arr.length;i++){
+            sum += arr[i];
+            if (map.containsKey(sum-k)){
+                len = Math.max(len,i-map.get(sum-k));
+            }
+            if (!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+        }
+        return len;
+    }
+
+
 }
