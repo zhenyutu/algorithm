88dd98b74c83d8f11f06e4ce2033780baf64f838
zhenyutu <1572629866@qq.com>
2017-11-26 09:52:26 +0800

    完成有环链表的环入口

---
+++ src/cn/tzy/app/swordoffer/EntryNodeOfLoopList.java
@@ -0,0 +1,30 @@
+package cn.tzy.app.swordoffer;
+
+import com.sun.xml.internal.ws.addressing.WsaTubeHelperImpl;
+
+/**
+ * Created by tuzhenyu on 17-11-26.
+ */
+public class EntryNodeOfLoopList {
+    public ListNode EntryNodeOfLoop(ListNode pHead)
+    {
+        if (pHead==null)
+            return null;
+        ListNode slow = pHead.next;
+        if (slow==null)
+            return null;
+        ListNode fast = pHead.next.next;
+        while (fast!=slow){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode pos = fast;
+        fast = pHead;
+        while (fast!=slow){
+            fast = fast.next;
+            slow = slow.next;   
+        }
+
+        return fast;
+    }
+}
