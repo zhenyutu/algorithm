96af3de242f4256d2e982d4ec87d313a65042c08
zhenyutu <1572629866@qq.com>
2018-01-02 10:29:04 +0800

    暴力递归，记忆搜索，动态规划完成换取钱币

--- src/cn/tzy/app/coderInterviewGuide2/chapter4/LeastCoins.java
+++ src/cn/tzy/app/coderInterviewGuide2/chapter4/LeastCoins.java
@@ -5,7 +5,7 @@ package cn.tzy.app.coderInterviewGuide2.chapter4;
  * @author tuzhenyu
  */
 public class LeastCoins {
-    public int leastCoins(int[] arr,int arm){
+    public int leastCoins0(int[] arr,int arm){
         if (arr==null||arr.length==0||arm<0)
             return -1;
         int[] dp = new int[arm+1];
@@ -21,5 +21,101 @@ public class LeastCoins {
         }
 
         return dp[arm];
+    }
+
+    /**
+     * 暴力递归
+     * @param arr
+     * @param arm
+     * @return
+     */
+    public int leastCoins1(int[] arr,int arm){
+        if (arr==null||arr.length==0||arm<0)
+            return -1;
+        return process1(arr,0,arm);
+    }
+
+    private int process1(int[] arr,int index,int arm){
+        int res = 0;
+        if (index==arr.length){
+            res=arm==0?1:0;
+        }else {
+            for (int i=0;arr[index]*i<=arm;i++){
+                res += process1(arr,index+1,arm-arr[index]*i);
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * 记忆搜索
+     * @param arr
+     * @param arm
+     * @return
+     */
+    public int leastCoins2(int[] arr,int arm){
+        if (arr==null||arr.length==0||arm<0)
+            return -1;
+        int[][] map = new int[arr.length+1][arm+1];
+        return process2(arr,0,arm,map);
+    }
+
+    private int process2(int[] arr,int index,int arm,int[][] map){
+        int res = 0;
+        if (index==arr.length){
+            res=arm==0?1:0;
+        }else {
+            int mapValue = 0;
+            for (int i=0;arr[index]*i<=arm;i++){
+                mapValue = map[index+1][arm-arr[index]*i];
+                if (mapValue!=0)
+                    res += mapValue==-1?0:mapValue;
+                else
+                    res += process1(arr,index+1,arm-arr[index]*i);
+            }
+
+        }
+        map[index][arm] = res==0?-1:res;
+        return res;
+    }
+
+    /**
+     * 动态规划
+     * @param arr
+     * @param arm
+     * @return
+     */
+    public int leastCoins3(int[] arr,int arm){
+        if (arr==null||arr.length==0||arm<0)
+            return -1;
+        int[][] dp = new int[arr.length][arm+1];
+
+        for (int i=0;i<arr.length;i++){
+            dp[i][0]=1;
+        }
+        for (int j=1;j*arr[0]<=arm;j++){
+                dp[0][j*arr[0]] = 1;
+        }
+        int num = 0;
+        for (int i=1;i<arr.length;i++){
+            for (int j=1;j<=arm;j++){
+                num = 0;
+                for (int k=0;j-k*arr[i]>=0;k++){
+                    num += dp[i-1][j-k*arr[i]];
+                }
+                dp[i][j] = num;
+            }
+        }
+
+        return dp[arr.length-1][arm];
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5,10,25,1};
+        int res = 15;
+        LeastCoins l = new LeastCoins();
+        System.out.println(l.leastCoins3(arr,res));
     }
 }
