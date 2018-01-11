5f292b5facfe2ced60321a93c4a0bcfc8d03d8ad
zhenyutu <1572629866@qq.com>
2018-01-11 11:00:59 +0800

    蓄水池问题

--- src/cn/tzy/app/coderInterviewGuide/chapter9/XuShuiChiQuestion.java
+++ src/cn/tzy/app/coderInterviewGuide/chapter9/XuShuiChiQuestion.java
@@ -5,4 +5,22 @@ package cn.tzy.app.coderInterviewGuide.chapter9;
  * @author tuzhenyu
  */
 public class XuShuiChiQuestion {
+    public int[] pick(int target,int num){
+        if (num<1||target<1)
+            return null;
+        int[] res = new int[Math.min(target,num)];
+        for (int i=0;i<res.length;i++){
+            res[i] = i+1;
+        }
+        for (int j=target+1;j<num+1;j++){
+            if (rand(j)<target){
+                res[rand(target)-1] = j;
+            }
+        }
+        return res;
+    }
+
+    private int rand(int max){
+        return (int)(Math.random()*max)+1;
+    }
 }
