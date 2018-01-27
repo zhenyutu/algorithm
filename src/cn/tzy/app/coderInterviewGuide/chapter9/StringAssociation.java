30d3c0e9560c907d32d19b86d74a61dc2ce8266c
zhenyutu <1572629866@qq.com>
2018-01-27 10:48:43 +0800

    完成全组合

--- src/cn/tzy/app/coderInterviewGuide/chapter9/StringAssociation.java
+++ src/cn/tzy/app/coderInterviewGuide/chapter9/StringAssociation.java
@@ -5,4 +5,24 @@ package cn.tzy.app.coderInterviewGuide.chapter9;
  * @author tuzhenyu
  */
 public class StringAssociation {
+    public void association(String str){
+        if (str==null||str.length()==0)
+            return;
+        int len = str.length();
+        int n = 1<<len;
+        for (int i=1;i<n;i++){
+            StringBuffer sb = new StringBuffer();
+            for (int j=0;j<len;j++){
+                if ((i&(1<<j))!=0){
+                    sb.append(str.charAt(j));
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        StringAssociation s = new StringAssociation();
+        s.association("123");
+    }
 }
